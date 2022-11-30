99b06b6ee02d102b85c587052b1101c87e32aa28
samuelkepler <57400752+samuelkepler@users.noreply.github.com>
2022-11-30 16:20:12 -0300

    Create JovemViajante.java

---
+++ JovemViajante.java
@@ -0,0 +1,30 @@
+public class JovemViajante {
+String name;
+String [] raca;
+String arma;
+String pet;
+Float mochila;
+Float bottle;
+String power;
+
+JovemViajante()
+{
+
+}
+
+
+public String getArma() {
+    return arma;
+}
+
+public String getPet() {
+    return pet;
+}
+
+public String[] getRaca() {
+    return raca;
+}
+
+public String getPower() {
+    return power;
+}
