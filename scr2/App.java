d6bfd832ea86b5a3169b73e69f50bcdbc3346ed6
samuelkepler <samuel.kepler@gmail.com>
2023-04-28 15:49:17 -0300

    ajustes na historia

--- scr2/App.java
+++ scr2/App.java
@@ -4,9 +4,9 @@ public class App{
 
 public static void main(String[] args) throws Exception {
         Scanner ler = new Scanner(System.in);
-
-        Personagem heroi = new Personagem("Caboclo");
-        Personagem vilao = new Personagem("Poli", 2000);
+        
+        Personagem zecaCalango = new Personagem("Zeca Calango", 0);
+        Personagem delegadoAssuncao = new Personagem("Delegado asunção", 2000);
        
-}
+ }
 }
--- scr2/Personagem.java
+++ scr2/Personagem.java
@@ -2,18 +2,30 @@ public class Personagem {
     String nome;
     int energia;
 
-    public Personagem(String nome) {
-        this.nome = nome;
-        this.energia = 1000;
-    }
-
-    public Personagem(String nome, int energia) {
+    Personagem(String nome, int energia) 
+    {
         this.nome = nome;
         this.energia = energia;
     }
 
-    void mostras() {
-        System.out.println();
+    void mudarEnergia(int mudarEnergia)
+    {
+        this.energia = this.energia + mudarEnergia;
+
+        if(this.energia > 100)
+        {
+            this.energia = 100;
+        }
+        else if(this.energia < 0)
+        {
+            this.energia = 0;
+            System.out.println("Perdendo todas as suas forças" + this.nome + 
+            ",  nosso amado heroi desfalece indo para a terra de pé junto" );
+
+            
+        
+        }
     }
+
 
 }
